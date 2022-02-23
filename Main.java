9c048d995a2b2e1abbd624149c2cd32ec7c76a3e
Marcin Lewicki <lemarcin@gmail.com>
2022-02-23 15:20:58 -0500

    Work on project. Stage 2/8: Duck numbers

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 class Main {
@@ -5,28 +6,39 @@ class Main {
         System.out.println("Enter a natural number:");
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        int n;
         if (input.matches("[1-9][0-9]*")) {
-            int n = Integer.parseInt(input);
-            if (n % 2 == 0) {
-                System.out.println("This number is Even.");
-            } else {
-                System.out.println("This number is Odd.");
-            }
-            if (n % 10 == 7 || n % 7 == 0) {
-                System.out.println("It is a Buzz number.\nExplanation:");
-                if (n % 10 == 7 && n % 7 == 0) {
-                    System.out.println(n + " is divisible by 7 and ends with 7.");
-                } else if (n % 7 == 0) {
-                    System.out.println(n + " is divisible by 7.");
-                } else if (n % 10 == 7) {
-                    System.out.println(n + " ends with 7.");
-                }
-            } else {
-                System.out.println("It is not a Buzz number.\nExplanation:");
-                System.out.println(n + " is neither divisible by 7 nor does it end with 7.");
-            }
+            n = Integer.parseInt(input);
+            System.out.println("Properties of " + n);
+            System.out.println("        even: " + even(n));
+            System.out.println("         odd: " + odd(n));
+            System.out.println("        buzz: " + buzz(n));
+            System.out.println("        duck: " + duck(n));
+
         } else {
             System.out.println("This number is not natural!");
         }
+    }
+
+    static boolean even(int n) {
+        return n % 2 == 0;
+    }
+
+    static boolean odd(int n) {
+        return n % 2 != 0;
+    }
+
+    static boolean buzz(int n) {
+        return (n % 10 == 7 || n % 7 == 0);
+    }
+
+    static boolean duck(int n) {
+        for (int rest = n; rest > 0; rest /= 10) {
+            int digit = rest % 10;
+            if (digit == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
